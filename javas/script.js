66abf4647ef3dd5e7da06c4f6ae213601a004f98
Christiano0407 <placebo0704@gmail.com>
2019-12-23 16:28:22 -0600

    var

--- javas/script.js
+++ javas/script.js
@@ -9,3 +9,10 @@ edad = 27;
 
 console.log("Hola, yo soy" + " " + nombre + " " + apellido);
 console.log('Mi edad'+ ' ' + edad);
+
+var mayuscula = nombre.toUpperCase();
+var minuscula = apellido.toLowerCase();
+var letra = nombre.charAt(0);
+var numero = apellido.length;
+
+var nombreCompleto =`${nombre}  ${apellido} `;
