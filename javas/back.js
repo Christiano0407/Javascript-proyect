2bf1e0b0408d0a8b90c4c8cb1af90fcebb474a59
Christiano0407 <placebo0704@gmail.com>
2020-01-02 09:13:47 -0600

    call

--- javas/back.js
+++ javas/back.js
@@ -1,1 +1,19 @@
-console.log(`Hola!`);
+/* console.log(`Hola!`); */
+
+const API_URL = `https://swapi.co/api/`;
+const PEOPLE_URL = `people/:id`;
+
+const lukeUrl = `${API_URL}${PEOPLE_URL.replace(`:id`, 1)}`
+const options = {crossDomain: true}
+
+const onPeopleResponse = function(persona) {
+    console.log(`Hola, soy ${persona.name}`)
+}
+
+$.get(lukeUrl, options, onPeopleResponse);
+
+
+
+
+
+
