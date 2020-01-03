971e44d6bb6e41b19443e1f7c4031940b3330cce
Christiano0407 <placebo0704@gmail.com>
2020-01-03 09:59:50 -0600

    star

--- javas/back.js
+++ javas/back.js
@@ -1,6 +1,6 @@
 /* console.log(`Hola!`); */
 
-const API_URL = `https://swapi.co/api/`;
+/* const API_URL = `https://swapi.co/api/`;
 const PEOPLE_URL = `people/:id`;
 
 const lukeUrl = `${API_URL}${PEOPLE_URL.replace(`:id`, 1)}`
@@ -10,10 +10,39 @@ const onPeopleResponse = function(persona) {
     console.log(`Hola, soy ${persona.name}`)
 }
 
-$.get(lukeUrl, options, onPeopleResponse);
+$.get(lukeUrl, options, onPeopleResponse); */
 
 
+const API_URL = `https://swapi.co/api/`
+const PEOPLE_URL = `people/:id`
+
+const LUKEURL = `${API_URL}${PEOPLE_URL.replace(`:id`, 1)}`
+const OPTION = {crossDomain:true}
+
+$.get(LUKEURL,OPTION, function(luke){
+    console.log(`Hola, me llamo ${luke.name}`)
+});
+
+const API_URLS = `https://swapi.co/api/`
+const DARK_URL = `people/:id`
+
+const VADERURL = `${API_URLS}${DARK_URL.replace(`:id`, 4)}`
+const OPT = {crossDomains:true}
+
+$.get(VADERURL, OPT, function(sith){
+    console.log(`Vuelo al lado oscuro, porque, soy ${sith.name}`)
+})
 
 
+const APIS_URL = `https://swapi.co/api/`
+const GIRL_URL = `people/:id`
 
+const LEIAURL = `${APIS_URL}${GIRL_URL.replace(`:id`, 5)}`
+const OPTS = {crossDomain: true}
+
+const FORCE = function(leia){
+ console.log(`Hola, soy hermana de Luke; me llamo ${leia.name}`)
+}
+
+$.get(LEIAURL, OPTS, FORCE)
 
