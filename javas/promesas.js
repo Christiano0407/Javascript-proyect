7a69b5390340aaa5ac507250a2282cd8304e5bd2
Christiano0407 <placebo0704@gmail.com>
2020-01-06 06:29:05 -0600

    promesa

---
+++ javas/promesas.js
@@ -0,0 +1,30 @@
+ const API_URL = `https://swapi.co/api/`
+ const PEOPLE_URL = `people/id`
+ const OPTS = {crossDomain:true}
+
+ 
+
+function obtenerPersonaje(id) {
+    return new Promise((resolve, reject) => {
+        const URL = `${API_URL}${PEOPLE_URL.replace(`:id`, id)}`
+        
+        $.get(URL, OPTS, function(data){
+
+            resolve(data)
+        })
+        .fail(() => reject(id))
+    })
+}
+
+/* function onError(id){
+ console.log(`Hubo un error ${id}`)
+} */
+
+
+obtenerPersonaje(1)
+.then( function(personaje){
+    console.log(`El personaje uno es: ${personaje.name}`)
+})
+.catch( function(id){
+    console.log(`Hubo un error`)
+})
