b4af98be1c0b61b0fdf9805df5ffc9ad4f49fc2e
Christiano0407 <placebo0704@gmail.com>
2020-04-07 12:47:19 -0500

    break

--- javas/app.js
+++ javas/app.js
@@ -3,25 +3,42 @@ var Mau = {
     edad: 28,
     peso: 50
 }
-/* console.log(`Todavía tengo ${Mau.edad} años`)
+console.log(`Todavía tengo ${Mau.edad} años`)
 const EDADMAYOR = 30;
+const EDADMENOR = 28;
 const DIAS_ANO = 365;
+var Premier = prompt (`Quién realizó la mejor transferencia en el mercado...?`)
 
-menor = () => {
-    if(Mau.edad < EDADMAYOR){
-        console.log(`Todavía soy joven!`)
-    }else {
-        console.log(`Demonios!`)
-    }
-} 
-
-for (var i = 1; i <= DIAS_ANO; i++){
-
-    var random = Math.random();
-    if(random < 0.25){
-        menor(Mau);
-    }else if (random < 0.5)
-    console.log(`No!`)
-}
-console.log(`Todavía tengo ${Mau.edad} años`) */
-/* menor(Mau); */
+switch(Premier) {
+    case `Arsenal`:
+        console.log(`Gabriel Martinelli (Ituano) Undisclosed Details
+        Dani Ceballos (Real Madrid) Loan Details
+        William Saliba (Saint-Etienne) Undisclosed Details`)
+    break
+     case `Aston Villa`:
+         console.log(`Jota (Birmingham) Undisclosed Details
+         Anwar El Ghazi (Lille) Undisclosed Details
+         Wesley (Club Brugge) Undisclosed Details`)
+    break 
+    case `Chelsea`:
+        console.log(`Mateo Kovacic (Real Madrid) Undisclosed Details`)  
+    break
+    case `Everton`:
+        console.log(`Jonas Lossl (Huddersfield Town) Free Details
+        Andre Gomes (Barcelona) £22m Details
+        Fabian Delph (Man City) Undisclosed Details`)  
+    break 
+    case `Liverpool`:
+         console.log(`Jonas Lossl (Huddersfield Town) Free Details
+         Andre Gomes (Barcelona) £22m Details
+         Fabian Delph (Man City) Undisclosed Details`)
+    break
+    case `Manchester United`:
+        console.log(`Daniel James (Swansea City) Undisclosed Details
+        Aaron Wan-Bissaka (Crystal Palace) Undisclosed Details
+        Harry Maguire (Leicester City) Undisclosed Details`)
+        break
+    default:
+        console.log(`No es un equipo de la Premier`)
+        break
+};
